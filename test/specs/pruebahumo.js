985e118d68161ccc4d4a278b805fe6b6b1e4b6e3
Daniela Brito <daniela.brito@arkanosoft.com>
2022-05-29 18:45:06 -0400

    Se agrega Await en maxWindow, btn.click. se elimina url en el browser.url, se elimina browser pause, cambio de navegador

--- test/specs/pruebahumo.js
+++ test/specs/pruebahumo.js
@@ -4,33 +4,17 @@ describe('OpenCart', () => {
 
     it('login bank', async () => {
 
-      browser.maximizeWindow()
-      await browser.url('http://localhost:8080/bank');
+      await browser.maximizeWindow()
+      await browser.url('');
       let user = await $('#username');
       await user.setValue('jsmith@demo.io');
       let pass = await $('#password');
       await pass.setValue('Demo123!');
       let btn = await $('#submit');
-      btn.click();
+      await btn.click();
       let panel = await $('#balanceSummary')
       await expect(panel).toExist()
-      await browser.pause(10000)
       
-    
     });
-
-    it('Revisar Cuenta', async () => {
-
-        let cuentas = await $('.dropdown-toggle');
-        await cuentas.click();
-        let vercta = await $('#view-checking-menu-item')
-        await vercta.click();
-        let inf = await $('.text-light=Account Number: 486130005');
-        await browser.pause(5000)
-        expect(await inf.getText()).toEqual('Account Number: 486130005')
-        
-      
-      });
-
     
  });
--- wdio.conf.js
+++ wdio.conf.js
@@ -56,7 +56,7 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 5,
         //
-        browserName: 'chrome',
+        browserName: 'firefox',
         acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
@@ -94,7 +94,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'http://localhost:8080/bank',
+    baseUrl: 'http://localhost:8080/bank/',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
@@ -110,7 +110,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
+    services: ['geckodriver'], 
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
