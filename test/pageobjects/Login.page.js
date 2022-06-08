d11870b12a01f70e4b3ce1a4a4f47afc8a029ef8
Daniela Brito <daniela.brito@arkanosoft.com>
2022-06-08 18:34:16 -0400

    Se cambia nombre de la clase de Login a LoginPage, se renombran los nombres de los atributos, cambiando nombre y estructura de los metodos a usar de login

--- test/pageobjects/Login.page.js
+++ test/pageobjects/Login.page.js
@@ -1,50 +1,40 @@
 
 import BasePage from '../pageobjects/base.page'
 
-class Login extends BasePage{
+class LoginPage extends BasePage{
 
     //WebElements
-    get principal(){return $('.align-content')}
-    get tittle(){return $('.login-logo')}
-    get usernamein(){ return $('#username') }
-    get passwordin(){return $('#password')}
-    get rememberme(){return $('#remember-me')}
-    get signin(){return $('#submit')}
-    get signuphere(){return $('=Sign Up Here')}
-    get flash() {return $('.badge')}
+    get title(){return $('.login-logo')}
+    get usernameInput(){ return $('#username') }
+    get passwordInput(){return $('#password')}
+    get rememberMeCheckBox(){return $('#remember-me')}
+    get signInButton(){return $('#submit')}
+    get signUpHere(){return $('=Sign Up Here')}
+    get loginStateFeedback() {return $('.badge')}
 
     /**Metodos para iniciar sesion en Digital bank
      * @param {String} user
      * @param {String} password
      * 
      */
-    async loguearclick(username,password){
-        await this.vaciarCampoYEnviarTexto(await this.usernamein,username)
-        await this.vaciarCampoYEnviarTexto(await this.passwordin,password);
-        await this.clickearElemento(await this.signin);
-        
+    async ingresarCredenciales(username,password){
+        await this.vaciarCampoYEnviarTexto(await this.usernameInput,username)
+        await this.vaciarCampoYEnviarTexto(await this.passwordInput,password);
     }
 
-   /*
-    async loguearenter(username,password){
-        await this.usernamein(username).keys('tab')
-        await this.passwordin(password).keys('tab')
-        await this.signin.keys('enter')
-    }*/
-
-    async loguearclickreme(username,password){
-        await this.vaciarCampoYEnviarTexto(await this.usernamein,username)
-        await this.vaciarCampoYEnviarTexto(await this.passwordin,password);
-        await this.clickearElemento(await this.rememberme)
-        await this.clickearElemento(await this.signin);
+    async ClickEnBotonSubmit(){
+        await this.clickearElemento(await this.signInButton);  
     }
 
-    
+    async ClickEnCheckboxRememberMe(){
+        await this.clickearElemento(await this.rememberMeCheckBox)
+    }
 
-    async crearcta(){
-        await this.clickearElemento(await this.signuphere);
+
+    async irAPaginaDeSignUp(){
+        await this.clickearElemento(await this.signUpHere);
     }
   
 
 }
-export default new Login();
+export default new LoginPage();
