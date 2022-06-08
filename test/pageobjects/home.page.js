de39d244a6eff25a28f4fda6e9d4bda099c49f1e
Daniela Brito <daniela.brito@arkanosoft.com>
2022-06-08 18:32:19 -0400

    Se cambia nombre de atributo tittle a title, tambien eliminando comentarios

--- test/pageobjects/home.page.js
+++ test/pageobjects/home.page.js
@@ -3,26 +3,17 @@ import BasePage from '../pageobjects/base.page';
 class HomePage extends BasePage{
 
     //Elementos Web
-    get tittle(){return $('#page-title')}
+    get title(){return $('#page-title')}
     get avatar(){return $('.user-avatar')}
     get logout(){return $('.fa-power-off')}
 
-   //-------------------------------------------------------------------------------------------------------//
- 
-   /**
-    * Click en el resultado de la búsqueda
-    */
-   async salirHome() {
+   
+   async desloguearse() {
        await super.clickearElemento(await this.avatar);
        await super.clickearElemento(await this.logout);
    }
 
-   /**
-    * Obtener texto del resultado de la búsqueda
-    
-   async obtenerNombreResultado() {
-       await this.tittle.getText();
-          }*/
+  
 
 
 }
