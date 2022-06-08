ea70e68a8aae3e81426eec0daf9ce6a9e5692334
Daniela Brito <daniela.brito@arkanosoft.com>
2022-06-08 18:30:08 -0400

    Se eliminan lineas entre metodo y cambio de estructura de metodo vaciarCampoYEnviarTexto

--- test/pageobjects/base.page.js
+++ test/pageobjects/base.page.js
@@ -20,20 +20,21 @@ export default class BasePage {
    async clickearElemento(elemento) {
        await elemento.waitForClickable({ timeout: PAGE_TIMEOUT });
        await elemento.click();
+     
    }
 
-
+ 
    /**
     * Método para enviar texto a un elemento
     * @param {Object} elemento que recibirá el texto
     * @param {String} texto a envíar 
     */
-   async vaciarCampoYEnviarTexto(elemento, texto){
-       await elemento.waitForClickable({ timeout: PAGE_TIMEOUT });
-       await elemento.clearValue();
-       await elemento.click();
-       await elemento.keys(texto);
-   }
+    async vaciarCampoYEnviarTexto(elemento, texto){
+        await elemento.waitForClickable({ timeout: PAGE_TIMEOUT });
+        await elemento.setValue(texto);
+      }
+
+
 
 
 }
