6279bfa871340cb408bbd13302a7b2ab80e1293f
Daniela Brito <daniela.brito@arkanosoft.com>
2022-06-09 10:33:50 -0400

    Cambio de nombre de los import de las clases y en el uso de ellas en cada test

--- test/specs/login.test.js
+++ test/specs/login.test.js
@@ -1,72 +1,72 @@
-import loginPage from '../pageobjects/Login.page';
-import homePage from '../pageobjects/home.page'
-import formu from '../pageobjects/form.page'
+import LoginPage from '../pageobjects/Login.page';
+import HomePage from '../pageobjects/home.page'
+import RegistroUserPage from '../pageobjects/registroUser.page'
 
 describe('Login Suite', () => {
   
     it('Deberia acceder a la pagina principal cuando se loguea con credenciales validas', async () => {
 
-      await loginPage.abrir('');      
+      await LoginPage.abrir('');      
       let username = 'jsmith@demo.io'
       let password = 'Demo123!'
-      await loginPage.ingresarCredenciales(username, password);
-      await loginPage.ClickEnBotonSubmit()
-      await expect(homePage.title).toExist();
+      await LoginPage.ingresarCredenciales(username, password);
+      await LoginPage.clickEnBotonSubmit()
+      await expect(HomePage.title).toExist();
             
     });
     
     it('Deberia no acceder a la pagina principal cuando se loguea con usuario invalido', async () => {
 
-      await loginPage.abrir('login');      
+      await LoginPage.abrir('login');      
       let username = 'dsmith@demo.io'
       let password = 'Demo123!'
-      await loginPage.ingresarCredenciales(username, password);
-      await loginPage.ClickEnBotonSubmit()
-      await expect(loginPage.loginStateFeedback).toHaveTextContaining('Error')
+      await LoginPage.ingresarCredenciales(username, password);
+      await LoginPage.clickEnBotonSubmit()
+      await expect(LoginPage.loginStateFeedback).toHaveTextContaining('Error')
             
     });
 
 
     it('Deberia no acceder a la pagina principal cuando se loguea con password invalida', async () => {
 
-      await loginPage.abrir('login');      
+      await LoginPage.abrir('login');      
       let username = 'jsmith@demo.io'
       let password = 'demo123!'
-      await loginPage.ingresarCredenciales(username, password);
-      await loginPage.ClickEnBotonSubmit()
-      await expect(loginPage.loginStateFeedback).toHaveTextContaining('Error')
+      await LoginPage.ingresarCredenciales(username, password);
+      await LoginPage.clickEnBotonSubmit()
+      await expect(LoginPage.loginStateFeedback).toHaveTextContaining('Error')
             
     });
 
     it('Deberia acceder a la pagina principal cuando se loguea con credenciales validas y marcando el checkbox Remember me', async () => {
 
-      await loginPage.abrir('login');      
+      await LoginPage.abrir('login');      
       let username = 'nsmith@demo.io'
       let password = 'Demo123!'
-      await loginPage.ingresarCredenciales(username, password);
-      await loginPage.ClickEnCheckboxRememberMe();
-      await loginPage.ClickEnBotonSubmit();
-      await expect(homePage.title).toExist();
+      await LoginPage.ingresarCredenciales(username, password);
+      await LoginPage.clickEnCheckboxRememberMe();
+      await LoginPage.clickEnBotonSubmit();
+      await expect(HomePage.title).toExist();
       
     });
 
     it('Deberia no acceder a la pagina principal cuando se dejan los campos vacios y seleccionando check box Remember me', async () => {
 
-      await loginPage.abrir('login');      
+      await LoginPage.abrir('login');      
       let username = ''
       let password = ''
-      await loginPage.ingresarCredenciales(username,password)
-      await loginPage.ClickEnCheckboxRememberMe();
-      await loginPage.ClickEnBotonSubmit();
-      await expect(loginPage.loginStateFeedback).toHaveTextContaining('Error')
+      await LoginPage.ingresarCredenciales(username,password)
+      await LoginPage.clickEnCheckboxRememberMe();
+      await LoginPage.clickEnBotonSubmit();
+      await expect(LoginPage.loginStateFeedback).toHaveTextContaining('Error')
       
     });
 
     it('Debería de acceder al formulario de registro', async () => {
 
-      await loginPage.abrir('login');      
-      await loginPage.irAPaginaDeSignUp();
-      await expect(formu.form).toExist();
+      await LoginPage.abrir('login');      
+      await LoginPage.irAPaginaDeSignUp();
+      await expect(RegistroUserPage.form).toExist();
       
     });
 
