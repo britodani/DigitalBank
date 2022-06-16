d250611aecea820b8d297e38743080ad3672acb6
Daniela Brito <daniela.brito@arkanosoft.com>
2022-06-16 09:38:10 -0400

    Modelos de datos correctos e incorrector para el ejercicio de data driven test

---
+++ test/datos/userypasscorrectos.js
@@ -0,0 +1,4 @@
+module.exports =[
+    {    username: 'jsmith@demo.io', password: 'Demo123!' },
+    {    username: 'nsmith@demo.io', password: 'Demo123!' }
+]
---
+++ test/datos/userypassincorrecto.js
@@ -0,0 +1,5 @@
+module.exports =[
+    {    username: 'dsmith@demo.io', password: 'Demo123!' },
+    {    username: 'jsmith@demo.io', password: 'demo123!' },
+    {    username: '', password: '' }
+]
