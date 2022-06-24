e2464014b940cb32e67dc7ed1bbb475dca33eb82
Daniela Brito <daniela.brito@arkanosoft.com>
2022-06-23 22:03:51 -0400

    Archivos de datos incorrectos y correctos

---
+++ test/datos/registroUser.js
@@ -0,0 +1,6 @@
+module.exports =[
+    {    title: 'Mr.', firstName: 'josue', lastName: 'rodriguez', genero: 'M', fecNac: '01/01/1992', nroSegSoc: '123-45-6789', email: 'josuerodriguez@gmail.com', password: 'Demo456¡', confPass: 'Demo456¡' },
+    {    title: 'Ms.', firstName: 'Sandra', lastName: 'mendez', genero: 'F', fecNac: '01/02/1993', nroSegSoc: '456-78-9012', email: 'sandramendez@hotmail.com', password: 'Demo456¡', confPass: 'Demo456¡' },
+    {    title: 'Mrs.', firstName: 'Ines', lastName: 'hernandez', genero: 'F', fecNac: '01/03/1994', nroSegSoc: '345-67-8901', email: 'ineshernandez@outlook.com', password: 'Demo456¡', confPass: 'Demo456¡' }
+   // { title: 'Mrs.', firstName: 'Ines', lastName: 'hernandez', fecNac: '01/03/1994', nroSegSoc: '345-67-8901', email: 'ineshernandez@outlook.com', password: 'Demo456¡', confPass: 'Demo456¡'}
+]
