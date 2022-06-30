f2a774e4ebe02c36599d332520bde2054564d8dc
Daniela Brito <daniela.brito@arkanosoft.com>
2022-06-30 10:58:51 -0400

    Archivos de datos para la creacion de cuenta y fallo

---
+++ test/datos/newChecking.js
@@ -0,0 +1,6 @@
+module.exports =[
+    {    cuenta: '8', titularidad: '18', accName: 'Cta Auto', iniMount: '25'},
+    {    cuenta: '8', titularidad: '17', accName: 'Cta Vivienda', iniMount: '300'},
+    {    cuenta: '9', titularidad: '17', accName: 'Cta Nomina', iniMount: '1000'},
+     
+]
